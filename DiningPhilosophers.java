bec6c061e1aa186634dad15afde92343a7bd032a
proximaCent <sabikaftahee01@gmail.com>
2024-09-25 16:35:57 +0600

    tried to do it with OOP

--- DiningPhilosophers.java
+++ DiningPhilosophers.java
@@ -1,38 +1,50 @@
-import java.util.Random;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class DiningPhilosophers {
-    private static final int NUM_TABLES = 6;
-    private static final int NUM_PHILOSOPHERS = 5; 
-    private final ReentrantLock[][] forks = new ReentrantLock[NUM_TABLES][NUM_PHILOSOPHERS]; 
-    private final boolean[][] seats = new boolean[NUM_TABLES][NUM_PHILOSOPHERS]; 
-    
 
-    public DiningPhilosophers() {
-        for (int table = 0; table < NUM_TABLES; table++) {
-            for (int seat = 0; seat < NUM_PHILOSOPHERS; seat++) {
-                forks[table][seat] = new ReentrantLock();
-                seats[table][seat] = false;
-            }
+    public static void main(String[] args) {
+        int numPhilosophers = 5;
+        Phil[] philosophers = new Phil[numPhilosophers];
+        Fork[] forks = new Fork[numPhilosophers];
+
+        for (int i = 0; i < numPhilosophers; i++) {
+            forks[i] = new Fork(i);
+            philosophers[i] = new Phil(i, forks[i], forks[(i + 1) % numPhilosophers]); //philosopher and fgorks
+        }
+
+        
+        for (int i = 0; i < numPhilosophers; i++) {
+            philosophers[i].eat(); //in each iteration the will try to eat
         }
     }
+}
 
+class Phil {
+    private int id;
+    private Fork lFork;
+    private Fork rFork;
 
-    public void startDining() {
-        Thread[] philosopherThreads = new Thread[NUM_TABLES * NUM_PHILOSOPHERS];
-
-        // philosopher threads for tables
-        for (int table = 0; table < NUM_TABLES - 1; table++) {
-            for (int seat = 0; seat < NUM_PHILOSOPHERS; seat++) {
-                char philosopherLabel = (char) ('A' + (table * NUM_PHILOSOPHERS) + seat);
-                philosopherThreads[(table * NUM_PHILOSOPHERS) + seat] = new Thread(new Philosopher(table, seat, philosopherLabel));
-                philosopherThreads[(table * NUM_PHILOSOPHERS) + seat].start();
-                seats[table][seat] = true; 
-            }
-        
-
-        System.out.println("Simulation donee");
-        System.out.println("The last philosopher who moved to the sixth table was: " + lastMovedPhilosopher);
+    public Phil(int id, Fork lFork, Fork rFork) {
+        this.id = id;
+        this.lFork = lFork;
+        this.rFork = rFork;
     }
 
-   
+    public void eat() {
+        System.out.println("Philosopher " + id + " is trying to pick up forks.");
+        System.out.println("Philosopher " + id + " picked up left fork " + lFork.getId());
+        System.out.println("Philosopher " + id + " picked up right fork " + rFork.getId());
+        System.out.println("Philosopher " + id + " is eating.");
+        System.out.println("Philosopher " + id + " finished eating and put down the forks.");
+    }
+}
+
+class Fork {
+    private int id;
+
+    public Fork(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
