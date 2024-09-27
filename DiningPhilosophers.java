651dca2fae3f4205c42509fb46dcec3a958b921a
proximaCent <sabikaftahee01@gmail.com>
2024-09-27 22:13:43 +0600

    If a philosopher fails to acquire both forks, they will wait for a random amount of time before retrying

--- DiningPhilosophers.java
+++ DiningPhilosophers.java
@@ -4,15 +4,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class DiningPhilosophersStep5 {
+public class DiningPhilosophers {
     private static final int NUM_PHILOSOPHERS = 5;
     private static final int NUM_TABLES = 6; 
     private static final Random random = new Random();
     private static final int MAX_ATTEMPTS = 3; 
 
-    // Forks for each philosopher
+    // each philosopher per table
     private static final Semaphore[][] forks = new Semaphore[NUM_TABLES][NUM_PHILOSOPHERS];
-
     private static final int[] philosophersAtTable = new int[NUM_TABLES];
 
     // Deadlock detection flags
@@ -22,19 +21,18 @@ public class DiningPhilosophersStep5 {
     public static void main(String[] args) throws InterruptedException {
         for (int table = 0; table < NUM_TABLES; table++) {
             for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-                forks[table][i] = new Semaphore(1);  //held by 1 philosopher at a time
+                forks[table][i] = new Semaphore(1);  
             }
-        }
-
+        } 
         for (int i = 0; i < NUM_TABLES - 1; i++) {
-            philosophersAtTable[i] = NUM_PHILOSOPHERS; 
+            philosophersAtTable[i] = NUM_PHILOSOPHERS;  
         }
 
-        // Thread pool for philosopher
+        // Thread for managing philosopher
         ExecutorService executorService = Executors.newFixedThreadPool(NUM_PHILOSOPHERS * (NUM_TABLES - 1));
 
-        
-        char philosopherLabel = 'A'; 
+       
+        char philosopherLabel = 'A';  
         for (int table = 0; table < NUM_TABLES - 1; table++) {
             for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
                 executorService.submit(new Philosopher(philosopherLabel++, table, i));
@@ -52,7 +50,7 @@ public class DiningPhilosophersStep5 {
         private final char label;
         private final int position;
         private int currentTable;
-        private int failedAttempts = 0; 
+        private int failedAttempts = 0;  
         public Philosopher(char label, int currentTable, int position) {
             this.label = label;
             this.currentTable = currentTable;
@@ -70,7 +68,7 @@ public class DiningPhilosophersStep5 {
                             moveToSixthTable();
                             if (philosophersAtTable[5] == NUM_PHILOSOPHERS) {
                                 sixthTableDeadlocked = true;
-                                lastPhilosopherToMove = String.valueOf(label); 
+                                lastPhilosopherToMove = String.valueOf(label);  
                             }
                         }
                     } else {
@@ -81,38 +79,49 @@ public class DiningPhilosophersStep5 {
                 Thread.currentThread().interrupt();
             }
         }
+
+
         private void think() throws InterruptedException {
             System.out.println("Philosopher " + label + " is thinking at Table " + currentTable + "...");
-            Thread.sleep(random.nextInt(3000));
+            Thread.sleep(random.nextInt(3000)); 
         }
+
         private boolean eat() throws InterruptedException {
             int leftFork = position;
             int rightFork = (position + 1) % NUM_PHILOSOPHERS;
-            if (!forks[currentTable][leftFork].tryAcquire(1, TimeUnit.SECONDS)) {
-                return false;  
+
+           
+            if (!forks[currentTable][leftFork].tryAcquire(500, TimeUnit.MILLISECONDS)) {
+                return false; 
             }
-            if (!forks[currentTable][rightFork].tryAcquire(1, TimeUnit.SECONDS)) {
+            if (!forks[currentTable][rightFork].tryAcquire(500, TimeUnit.MILLISECONDS)) {
                 forks[currentTable][leftFork].release(); 
                 return false;
             }
 
             System.out.println("Philosopher " + label + " picked up both forks at Table " + currentTable + ".");
+
+            
             eatMeal();
             forks[currentTable][leftFork].release();
             forks[currentTable][rightFork].release();
             System.out.println("Philosopher " + label + " finished eating at Table " + currentTable + " and put down forks.");
             return true;
         }
+
+       
         private void eatMeal() throws InterruptedException {
             System.out.println("Philosopher " + label + " is eating at Table " + currentTable + ".");
-            Thread.sleep(random.nextInt(2000));
+            Thread.sleep(random.nextInt(2000));  
         }
+
+        // Move to the sixth table 
         private void moveToSixthTable() throws InterruptedException {
             synchronized (philosophersAtTable) {
-                if (philosophersAtTable[5] < NUM_PHILOSOPHERS) {  
+                if (philosophersAtTable[5] < NUM_PHILOSOPHERS) {  // Only move if the sixth table has space
                     System.out.println("Philosopher " + label + " is moving to the sixth table.");
                     philosophersAtTable[currentTable]--;
-                    currentTable = 5; 
+                    currentTable = 5;  // Move to the sixth table
                     philosophersAtTable[5]++;
                 }
             }
